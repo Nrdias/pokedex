c4cfde85b4819476ceacdef30db9f4b768c46310
Nrdias <natan.nrdias@hotmail.com>
2022-11-16 16:37:32 -0300

    main.js file updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,49 @@
-console.log("Hello, World!");
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+
+const maxRecords = 151;
+const limit = 10;
+let offset = 0;
+
+function convertPokemon(pokemon) {
+  return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types
+                      .map((type) => `<li class="type ${type}">${type}</li>`)
+                      .join("")}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `;
+}
+
+function loadPokemon(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map(convertPokemon).join("");
+    pokemonList.innerHTML += newHtml;
+  });
+}
+
+loadPokemon(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+  const qtdRecordsWithNexPage = offset + limit;
+
+  if (qtdRecordsWithNexPage >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemon(offset, newLimit);
+
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else {
+    loadPokemon(offset, limit);
+  }
+});
